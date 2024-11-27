629cc8cdf0c45c7cbe6020f4faebd6d11e004f97
dirtyharryiv <harfah87+github@gmail.com>
2024-11-27 22:06:08 +0000

    added version and CHANGELOG.md

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Changelog
+
+## [1.0.0] - 2024-11-27
+### Added
+- Implemented a new `coordinator` to manage ISS data fetching.
+- Fixed linter errors and improved code quality.
+- Updated `README.md` and changed HACS integration name.
+
+## [0.1.0] - 2024-11-26
+### Added
+- Initial commit for the ISS Spotter integration.
+- Basic setup of the custom component structure.
--- custom_components/iss_spotter/__init__.py
+++ custom_components/iss_spotter/__init__.py
@@ -4,12 +4,19 @@ Custom integration to integrate ISS Spot the Station.
 ISS Spot the Station NASA Website.
 """
 
+import logging
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+
+__version__ = "1.0.1"
+
+_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Spot Station from a config entry."""
+    _LOGGER.info("Spot Station Integration Version: %s", __version__)
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     )
