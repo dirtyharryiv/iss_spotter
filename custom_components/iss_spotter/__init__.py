08874c19cbba5f7d20ac0ba6bc9f379920a48e32
dirtyharryiv <harfah87+github@gmail.com>
2025-01-02 20:01:38 +0000

    new version

--- custom_components/iss_spotter/__init__.py
+++ custom_components/iss_spotter/__init__.py
@@ -9,7 +9,7 @@ import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-__version__ = "1.1.6"
+__version__ = "1.1.7"
 
 _LOGGER = logging.getLogger(__name__)
 
