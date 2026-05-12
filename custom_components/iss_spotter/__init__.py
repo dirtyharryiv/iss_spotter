226ba1ba35733d28780c35eaaa7c6704a7bb7f2f
Harald F. <harfah87+gitlab@gmail.com>
2026-05-12 20:19:21 +0000

    new version

--- custom_components/iss_spotter/__init__.py
+++ custom_components/iss_spotter/__init__.py
@@ -5,7 +5,7 @@ import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-__version__ = "2.3.0"
+__version__ = "2.3.1"
 
 _LOGGER = logging.getLogger(__name__)
 
